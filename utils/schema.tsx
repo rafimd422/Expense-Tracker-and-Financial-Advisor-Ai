7fce7f6e15aa7d8b007122e485b0af339387c99b
rafimd422 <rafimd2222@gmail.com>
2024-08-22 15:39:28 +0600

    created schema for budgets

---
+++ utils/schema.tsx
@@ -0,0 +1,12 @@
+import {
+integer, numeric, pgTable,serial,varchar
+} from 'drizzle-orm/pg-core'
+
+// budget schema
+export const Budgets = pgTable('budgets',{
+    id: serial('id',).primaryKey(),
+    name: varchar('name').notNull(),
+    amount: varchar('amount').notNull(),
+    Icon: varchar('icon'),
+    createdBy: varchar('createdBy').notNull()
+})
